13591ac84529b8f38d7cdc70740688ac831f5127
Filip Binkiewicz <filip.binkiewicz@gmail.com>
2017-04-11 18:50:50 +0200

    Code refactor

--- 1/interceptor.c
+++ 1/interceptor.c
@@ -8,58 +8,72 @@
 
 #include "interceptor.h"
 
+typedef Elf64_Ehdr      ehdr_t;
+typedef Elf64_Phdr      phdr_t;
+typedef Elf64_Sym       sym_t;
+typedef Elf64_Dyn       dyn_t;
+typedef Elf64_Rela      rela_t;
+typedef Elf64_Addr      addr_t;
+typedef Elf64_Xword     xword_t;
+
+
+
 typedef struct {
     const char *fname;
     void *faddr;
 } callback_communicator;
 
+static int is_vdso(const phdr_t *phdr) {
+    ehdr_t* ehdr_vdso = (ehdr_t *)getauxval(AT_SYSINFO_EHDR);
+    phdr_t* phdr_vdso =
+        (phdr_t *)(getauxval(AT_SYSINFO_EHDR) + ehdr_vdso->e_phoff);
+    if (phdr_vdso == phdr)
+        return 1;
+    return 0;
+}
+
 static int resolve_callback(struct dl_phdr_info *info, size_t size, void *_data) {
     int j;
 
     /* omit the vDSO library */
-    Elf64_Ehdr* ehdr_vdso = (Elf64_Ehdr *)getauxval(AT_SYSINFO_EHDR);
-    Elf64_Phdr* phdr_vdso =
-        (Elf64_Phdr *)(getauxval(AT_SYSINFO_EHDR) + ehdr_vdso->e_phoff);
-    if (phdr_vdso == info->dlpi_phdr)
+    if (is_vdso(info->dlpi_phdr))
         return 0;
 
     callback_communicator *data = (callback_communicator *)_data;
 
     for (j = 0; j < info->dlpi_phnum; j++) {
+        /* we only care about dynamic headers */
         if (info->dlpi_phdr[j].p_type != PT_DYNAMIC)
             continue;
 
-        Elf64_Dyn *dyn_hdr =
-            (Elf64_Dyn *)(info->dlpi_addr + info->dlpi_phdr[j].p_vaddr);
+        dyn_t *dyn_hdr =
+            (dyn_t *)(info->dlpi_addr + info->dlpi_phdr[j].p_vaddr);
         char *strtab;
-        Elf64_Sym *symtab;
+        sym_t *symtab_base;
 
         /* search for the symbol table and the string table in this segment */
         for (; dyn_hdr->d_tag != DT_NULL; dyn_hdr++) {
             if (dyn_hdr->d_tag == DT_STRTAB)
                 strtab = (char *)(dyn_hdr->d_un.d_ptr);
             if (dyn_hdr->d_tag == DT_SYMTAB)
-                symtab = (Elf64_Sym *)(dyn_hdr->d_un.d_ptr);
+                symtab_base = (sym_t *)(dyn_hdr->d_un.d_ptr);
         }
 
         /* use the fact that the string table is directly after the symbol table */
-        for (Elf64_Sym *ii = symtab; (char *)ii != strtab; ii++) {
-            if (ii->st_shndx == STN_UNDEF)
+        for (sym_t *sym = symtab_base; (char *)sym != strtab; sym++) {
+            if (sym->st_shndx == STN_UNDEF)
                 continue;
-            //printf("%d, STT_FUNC = %d\n", ii->st_info % 16);
 
-            char *symbol_name = strtab + ii->st_name;
+            char *symbol_name = strtab + sym->st_name;
             if (strcmp(symbol_name, (char *)data->fname) == 0) {
-                if ((ii->st_info % 16) == STT_FUNC) {
+                if ((sym->st_info % 16) == STT_FUNC) {
                     /* the first occurence of searched function is returned */
-                    data->faddr = (void *)(info->dlpi_addr + ii->st_value);
+                    data->faddr = (void *)(info->dlpi_addr + sym->st_value);
                 }
-                if ((ii->st_info % 16) == STT_GNU_IFUNC) {
-                    /*
-                     * in case of an indirect function,
-                     * we return its return value
-                     */
-                    void *(*ifun)(void) = (void *(*)())(info->dlpi_addr + ii->st_value);
+                if ((sym->st_info % 16) == STT_GNU_IFUNC) {
+                    /* for an indirect function return its return value */
+                    void *(*ifun)(void) =
+                        (void *(*)())(info->dlpi_addr + sym->st_value);
                     data->faddr = ifun();
                 }
                 return 1;
@@ -82,10 +96,7 @@ substitute_callback(struct dl_phdr_info *info, size_t size, void *_data) {
     int j;
 
     /* omit the vDSO library */
-    Elf64_Ehdr* ehdr_vdso = (Elf64_Ehdr *)getauxval(AT_SYSINFO_EHDR);
-    Elf64_Phdr* phdr_vdso =
-        (Elf64_Phdr *)(getauxval(AT_SYSINFO_EHDR) + ehdr_vdso->e_phoff);
-    if (phdr_vdso == info->dlpi_phdr)
+    if (is_vdso(info->dlpi_phdr))
         return 0;
 
     callback_communicator *data = (callback_communicator *)_data;
@@ -94,35 +105,35 @@ substitute_callback(struct dl_phdr_info *info, size_t size, void *_data) {
     for (j = 0; j < info->dlpi_phnum; j++) {
         if (info->dlpi_phdr[j].p_type != PT_DYNAMIC)
             continue;
-        Elf64_Dyn *dyn_hdr =
-            (Elf64_Dyn *)(info->dlpi_addr + info->dlpi_phdr[j].p_vaddr);
+        dyn_t *dyn_hdr =
+            (dyn_t *)(info->dlpi_addr + info->dlpi_phdr[j].p_vaddr);
         char *strtab;
-        Elf64_Sym *symtab;
-        Elf64_Rela *reloc;
-        Elf64_Xword nreloc;
+        sym_t *symtab_base;
+        rela_t *reloc_base;
+        xword_t nreloc;
 
         /* search for the string table, symbol table and relocation table */
         for (; dyn_hdr->d_tag != DT_NULL; dyn_hdr++) {
             if (dyn_hdr->d_tag == DT_STRTAB)
                 strtab = (char *)(dyn_hdr->d_un.d_ptr);
             if (dyn_hdr->d_tag == DT_SYMTAB)
-                symtab = (Elf64_Sym *)(dyn_hdr->d_un.d_ptr);
+                symtab_base = (sym_t *)(dyn_hdr->d_un.d_ptr);
             if (dyn_hdr->d_tag == DT_JMPREL)
-                reloc = (Elf64_Rela *)(dyn_hdr->d_un.d_ptr);
+                reloc_base = (rela_t *)(dyn_hdr->d_un.d_ptr);
             if (dyn_hdr->d_tag == DT_PLTRELSZ)
-                nreloc = (Elf64_Xword)(dyn_hdr->d_un.d_val / sizeof(Elf64_Rela));
+                nreloc = (xword_t)(dyn_hdr->d_un.d_val / sizeof(rela_t));
         }
 
-        for (Elf64_Rela *ii = reloc; ii != reloc + nreloc; ii++) {
-            if (ELF64_R_TYPE(ii->r_info) != R_X86_64_JUMP_SLOT)
+        for (rela_t *reloc = reloc_base; reloc != reloc_base + nreloc; reloc++) {
+            if (ELF64_R_TYPE(reloc->r_info) != R_X86_64_JUMP_SLOT)
                 continue;
-            Elf64_Sym *sym = symtab + ELF64_R_SYM(ii->r_info);
+            sym_t *sym = symtab_base + ELF64_R_SYM(reloc->r_info);
             char *symbol_name = strtab + sym->st_name;
 
             /* replace sought relocation with our own */
-            Elf64_Addr *reloc_addr = (Elf64_Addr *)(info->dlpi_addr + ii->r_offset);
+            addr_t *reloc_addr = (addr_t *)(info->dlpi_addr + reloc->r_offset);
             if (strcmp(symbol_name, data->fname) == 0) {
-                *reloc_addr = (Elf64_Addr)(data->faddr);
+                *reloc_addr = (addr_t)(data->faddr);
                 return 0;
             }
         }
